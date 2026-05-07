5b8c3496247efad49e975e8ecd5a94a579aeb67d
Lokesh Kumar <lokesh.s14032005@gmail.com>
2026-05-07 15:39:20 +0530

    Updated message showing based on latest timestamp

--- app/src/main/java/com/loki/chatapp/presentation/screen/chatscreen/ChatScreen.kt
+++ app/src/main/java/com/loki/chatapp/presentation/screen/chatscreen/ChatScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -17,9 +18,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.loki.chatapp.presentation.viewmodel.ChatViewModel
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -30,7 +34,7 @@ fun ChatScreen(
     onBack: () -> Unit
 ) {
     var message by remember { mutableStateOf("") }
-    val messages = viewModel.messages
+    val messages = viewModel.messages.sortedBy { it.timestamp }
     val currentUserId = FirebaseAuth.getInstance().currentUser?.uid ?: ""
 
     val chatId = listOf(currentUserId, userId)
@@ -40,8 +44,22 @@ fun ChatScreen(
         mutableStateOf(false)
     }
 
+    val listState = rememberLazyListState()
+
     LaunchedEffect(Unit) {
         viewModel.startListening(chatId)
+    }
+    var isFirstLoad by remember { mutableStateOf(true) }
+
+    LaunchedEffect(messages.size) {
+        if (messages.isNotEmpty()){
+            if(isFirstLoad){
+            listState.scrollToItem(messages.size - 1)
+                isFirstLoad=false
+            }else{
+                listState.animateScrollToItem(messages.size - 1)
+            }
+        }
     }
 
     Column(
@@ -72,6 +90,7 @@ fun ChatScreen(
 
         Divider(color = Color.White.copy(0.2f))
         LazyColumn(
+            state = listState,
             modifier = Modifier
                 .weight(1f)
                 .fillMaxWidth(),
@@ -80,21 +99,43 @@ fun ChatScreen(
             items(messages) { msg ->
                 val isMe = msg.senderId == currentUserId
 
+                val formattedTime = remember ( msg.timestamp ){
+                    try {
+                        val sdf = SimpleDateFormat("hh:mm a", Locale.getDefault())
+                        sdf.format(msg.timestamp)
+                    }catch (e:Exception){
+                        ""
+                    }
+                }
+
                 Row(
                     modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = if (isMe) Arrangement.End else Arrangement.Start
                 ) {
-                    Text(
-                        text = msg.text,
-                        color = Color.White,
-                        modifier = Modifier
-                            .background(
-                                if (isMe) Color(0xAA4CAF50)
-                                else Color.White.copy(alpha = 0.2f),
-                                shape = RoundedCornerShape(16.dp)
+                    Column(
+                        horizontalAlignment = if (isMe) Alignment.End else Alignment.Start
+                    ) {
+                        Text(
+                            text = msg.text,
+                            color = Color.White,
+                            modifier = Modifier
+                                .background(
+                                    if (isMe) Color(0xAA4CAF50)
+                                    else Color.White.copy(alpha = 0.2f),
+                                    shape = RoundedCornerShape(16.dp)
+                                )
+                                .padding(12.dp)
+                        )
+
+                        if(formattedTime.isNotEmpty()){
+                            Text(
+                                text = formattedTime,
+                                color = Color.White.copy(0.5f),
+                                fontSize = 11.sp,
+                                modifier = Modifier.padding(top = 4.dp)
                             )
-                            .padding(12.dp)
-                    )
+                        }
+                    }
                 }
                 Spacer(modifier = Modifier.height(6.dp))
             }
